d9715ff89b7b77a4785aa402670c29cfd8aeecee
Smith Chung <email@smithchung.com>
2017-08-17 14:22:47 -0700

    Updated Database Credentials

--- inc/config.php
+++ inc/config.php
@@ -1,13 +1,13 @@
 <?php
 // database credentials
-$db_host      = 'localhost';
-$db_name      = 'YOUR_DATABASE_NAME';
-$db_user      = 'YOUR_DATABASE_USERNAME';
-$db_user_pass = 'USERNAME_PASSWORD';
+$db_host      = 'ec2-23-21-246-11.compute-1.amazonaws.com';
+$db_name      = 'd9mllo92revvut';
+$db_user      = 'zapxzvnfzqodgi';
+$db_user_pass = '9706b74810d4daecc62549af4681953d645f3203aac072076dfc3e115e3f57a1';
 
 // base url, where the script will be installed
 // *please do not include a trailing slash*
-$baseurl = 'http://yoursite.com';
+$baseurl = 'https://vendordirectory.herokuapp.com/';
 
 // include global logic
 require_once('common.inc.php');
