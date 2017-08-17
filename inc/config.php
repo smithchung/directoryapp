1a45cd520afa5ef2dc2c2febe9866b26266cedfd
Smith Chung <email@smithchung.com>
2017-08-17 14:37:59 -0700

    Update config.php

--- inc/config.php
+++ inc/config.php
@@ -4,6 +4,7 @@ $db_host      = 'localhost';
 $db_name      = 'd9mllo92revvut';
 $db_user      = 'zapxzvnfzqodgi';
 $db_user_pass = '9706b74810d4daecc62549af4681953d645f3203aac072076dfc3e115e3f57a1';
+$db_port      = '5432'
 
 // base url, where the script will be installed
 // *please do not include a trailing slash*
